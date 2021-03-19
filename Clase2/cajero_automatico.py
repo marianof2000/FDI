9ae2c86f889545eb89d801f12af41ac9987ffa24
marianof2000 <mariano.francisco@gmail.com>
2021-03-19 20:15:47 -0300

    Update cajero_automatico.py

--- Clase2/cajero_automatico.py
+++ Clase2/cajero_automatico.py
@@ -1,34 +1,34 @@
 # Ejercicio Cajero Automático
 
 # Entrada
-importeRetirar= int(input("Ingrese Importe a Retirar: "))
+importe_a_retirar= int(input("Ingrese Importe a Retirar: "))
 # Variables
-billetes100 = 0
-billetes50 = 0
-billetes10 = 0
-billetes5 = 0
-billetes1 = 0
+billetes_100 = 0
+billetes_50 = 0
+billetes_10 = 0
+billetes_5 = 0
+billetes_1 = 0
 # Proceso
-billetes100 = importeRetirar // 100 # División sin resto
-resto = importeRetirar % 100 # Resto de la división
+billetes_100 = importe_a_retirar // 100 # División sin resto
+resto = importe_a_retirar % 100 # Resto de la división
 
-billetes50 = resto // 50
+billetes_50 = resto // 50
 resto = resto % 50
 
-billetes10 = resto // 10
+billetes_10 = resto // 10
 resto = resto % 10
 
-billetes5 = resto // 5
+billetes_5 = resto // 5
 resto = resto % 5
 
-billetes1 = resto // 1
+billetes_1 = resto // 1
 resto = resto % 1
 
 # Salida
 print("Billetes a Retirar")
 print("------------------")
-print("Billetes 100:", billetes100)
-print("Billetes 50:", billetes50)
-print("Billetes 10:", billetes10)
-print("Billetes 5:", billetes5)
-print("Billetes 1:", billetes1)
+print("Billetes 100: ", billetes_100)
+print("Billetes 50: ", billetes_50)
+print("Billetes 10: ", billetes_10)
+print("Billetes 5: ", billetes_5)
+print("Billetes 1: ", billetes_1)
